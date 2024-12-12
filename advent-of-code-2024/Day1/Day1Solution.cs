f27344090b325fdd75af85ddde0971979186b6c8
Gluckasz <lukas.gluck.04@gmail.com>
2024-12-12 20:57:47 +0100

    Add project files.

---
+++ advent-of-code-2024/Day1/Day1Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace advent_of_code_2024.Day1
+{
+    internal class Day1Solution
+    {
+        private List<int> leftList = new();
+        private List<int> rightList = new();
+
+        private void ReadData()
+        {
+            string[] lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "../../../Day1", "Input.txt"));
+            foreach (string line in lines)
+            {
+                string[] lineSplit = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                leftList.Add(Convert.ToInt32(lineSplit[0]));
+                rightList.Add(Convert.ToInt32(lineSplit[1]));
+            }
+        }
+
+        private void SaveResult(int result1, int result2)
+        {
+            File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "../../../Day1", "Output1.txt"), result1.ToString());
+            File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "../../../Day1", "Output2.txt"), result2.ToString());
+        }
+        public void Solution()
+        {
+            ReadData();
+            leftList.Sort();
+            rightList.Sort();
+            int sumOfDifferences = leftList.Zip(rightList, (a, b) => Math.Abs(a - b)).Sum();
+            int similarityScore = leftList.Sum(left => left * rightList.Count(right => right == left));
+            SaveResult(sumOfDifferences, similarityScore);
+        }
+    }
+}
---
+++ advent-of-code-2024/Program.cs
@@ -0,0 +1,13 @@
+﻿using advent_of_code_2024.Day1;
+
+namespace advent_of_code_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1Solution day1 = new();
+            day1.Solution();
+        }
+    }
+}
